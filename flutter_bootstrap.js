52daf1c00215a63592c4b5071ce8c614a56bb2c0
ShugoTenchii <MLKwi6057@gmail.com>
2025-04-22 21:58:34 +0200

    test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2170756505"
+    serviceWorkerVersion: "3546381115"
   }
 });
